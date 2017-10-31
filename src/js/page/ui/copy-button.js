6157d6412cac8e8162416825e32fd897cb76da79
Mihai Bercu <mihaiberq@gmail.com>
2017-10-31 12:56:59 +0000

    add width only if there's no width field

--- src/js/page/ui/copy-button.js
+++ src/js/page/ui/copy-button.js
@@ -23,7 +23,7 @@ class CopyButton {
 
   _onClick(event) {
     this._pre.textContent = '![alt description](' + encodeURIComponent(this._svgFile.text).replace(/\(/g, '%28')
-      .replace(/\)/g, '%29').replace('svg%20', 'svg%20width%3D%22100%25%22%20') + ')';
+      .replace(/\)/g, '%29').replace('svg%20height', 'svg%20width%3D%22100%25%22%20height') + ')';
     document.body.appendChild(this._pre);
     getSelection().removeAllRanges();
     var range = document.createRange();
